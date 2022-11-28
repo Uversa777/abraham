985622be67177498e00680fe654d868dd7644edf
Uversa777 <abrahamogah@yahoo.com>
2022-11-28 22:53:24 +0100

    homepage redesigned

---
+++ behaviour/index.js
@@ -0,0 +1,15 @@
+const navbarToggle = document.querySelector('#navbar-toggle');
+let isNavbarExpanded = navbarToggle.getAttribute('aria-expanded') === 'true';
+
+function toggleNavbarVisibility() {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute('aria-expanded', isNavbarExpanded);
+};
+
+navbarToggle.addEventListener('click', toggleNavbarVisibility);
+
+const navbarMenu = document.querySelector('#navbar-menu');
+const navbarLinksContainer = navbarMenu.querySelector('.navbar-links');
+
+navbarLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navbarMenu.addEventListener('click', toggleNavbarVisibility);
